1c9d0e85c67682e0de7e1c383b91a83e8e7cfedf
Jonathan Flowers <jmflowers134@gmail.com>
2019-04-23 12:27:30 -0400

    Update permissions.py

--- ecommerce/api/permissions.py
+++ ecommerce/api/permissions.py
@@ -12,15 +12,23 @@ class IsNotHacker(permissions.BasePermission):
         # The name that you need to evaluate is taken from `request.user.username`
         # in case it is a regular User, or from `request.user.name` in case it
         # is an APIClient.
-
-        return False
+        if isinstance(request.user, APIClient):
+            username = request.user.name 
+        else:
+            username = request.user.username
+        if 'hacker' in username.lower():
+            return False
+        else:
+            return True
 
 
 class IsOddProductID(permissions.BasePermission):
 
     # object level permission
     def has_object_permission(self, request, view, obj):
-        # check if the product id is and odd integer. If so, returns True.
+        # check if the product id is an odd integer. If so, returns True.
         # Otherwise return False.
-
-        return False
+        product = obj
+        if product.id % 2 == 0:
+            return False
+        return True
