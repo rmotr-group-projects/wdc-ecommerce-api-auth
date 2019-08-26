82cd7546f0f3adc898fbcf74ad7096aec54e9f5e
jpas <jordan.pa.schultz@gmail.com>
2019-08-26 03:17:39 +0000

    JSchultz 082519

--- ecommerce/api/views.py
+++ ecommerce/api/views.py
@@ -10,16 +10,10 @@ class ProductViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
 
-    # Define here the permissions for each endpoints according to the
-    # following conditions:
-
-    # - All endpoints will require the user to be authenticated
-    #   (see `IsAuthenticated` permission from DRF) and not being a hacker
-    #   (see `IsNotHacker` permission inside `api/permissions.py`)
-
-    # - All endpoints that modify the database will require the user to be admin
-    #   (see `IsAdminUser` permission from DRF).
-    #   This endpoints are the create, update, partial update and delete.
-
-    # - The retrieve endpoint will need the user to pass the `IsOddProductID`
-    #   permission (must also be implemented inside `api/permissions.py`)
+    def get_permissions(self):
+        permissions = [IsAuthenticated(), IsNotHacker()]
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            permissions += [IsAdminUser()]
+        elif self.action == 'retrieve':
+            permissions += [IsOddProductID()]
+        return permissions
